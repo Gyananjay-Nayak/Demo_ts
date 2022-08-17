81f0bb4eb840816dca6463095edb0a43c71195ca
Gyananjay Nayak <nayakgyananjay1999@gmail.com>
2022-08-17 16:14:45 +0530

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import "./App.css";
-import { Status } from "./components/Status";
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import Greet from "./components/Greet";
+import { Button } from "./components/button";
+import { Input } from "./components/Input";
 
 function App() {
   return (
     <div className="App">
-      <Status status="loading" />
-      <Heading>Placeholder Text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
-        <Greet name="Gyananjay" isLoggedIn={true} />
-      </Oscar>
+      <Button
+        handleClick={(event, id) => console.log("Button clicked!", event, id)}
+      ></Button>
+      <Input value="" handleChange={(event) => console.log(event.target.value)} />
     </div>
   );
 }
